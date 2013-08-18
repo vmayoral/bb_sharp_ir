beb6819a96307111a064fe9317396e6542502a2c
vmayoral <v.mayoralv@gmail.com>
2013-08-18 18:37:40 +0200

    new sensor

--- scripts/GP2Y0D810Z0F.py
+++ scripts/GP2Y0D810Z0F.py
@@ -3,16 +3,16 @@ import rospy
 from std_msgs.msg import String
 import Adafruit_BBIO.ADC as ADC
 
-# Sensor connected to P9_40
+# Sensor connected to P9_14
 
 def gp2y0a12():
     ADC.setup()
-    pub = rospy.Publisher('ir_sharp_80cm', String)
-    rospy.init_node('gp2y0a12_node')
+    pub = rospy.Publisher('ir_sharp_10cm', String)
+    rospy.init_node('GP2Y0D810Z0F_node')
     r = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         measure = str(ADC.read_raw("P9_40"))
-        cad = "gp2y0a12: %s" % measure
+        cad = "GP2Y0D810Z0F: %s" % measure
         rospy.loginfo(cad)
         pub.publish(String(measure))
         #rospy.sleep(1.0)
